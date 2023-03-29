ebdcb4659144907bec5fae266325b334c90bbe3c
Gorm2303 <gormdk333@gmail.com>
2023-03-29 16:36:29 +0200

    It is possible to upload files up to 60MB now, but not in chunks yet

--- app/app.py
+++ app/app.py
@@ -23,10 +23,8 @@ def index():
 def upload_video_metadata():
     # Parse JSON request body
     data = request.json
-
     # Insert video into MongoDB
     result = videosCollection.insert_one(data)
-
     # Return JSON response
     response = {
         'success': True,
@@ -35,7 +33,7 @@ def upload_video_metadata():
     }
     return jsonify(response)
 
-@app.route('/api/v1/poster', methods=['POST'])
+@app.route('/poster', methods=['POST'])
 @cross_origin()
 def upload_image():
     if 'file' not in request.files:
@@ -50,7 +48,7 @@ def upload_image():
     else:
         return jsonify({'error': 'No file found'})
 
-""" @app.route('/api/v1/video', methods=['POST'])
+@app.route('/video', methods=['POST'])
 @cross_origin()
 def upload_video():
     file = request.files['file']
@@ -61,53 +59,7 @@ def upload_video():
         file.save(file_path)
         return jsonify({'url': file_path})
     else:
-        return jsonify({'error': 'No file found'}) """
-
-@app.route('/api/v1/video', methods=['POST'])
-@cross_origin()
-def upload():
-    chunk_index = request.form['chunk-index']
-    total_chunks = request.form['total-chunk']
-    chunk_file = request.files['chunkFile']
-    file_name = chunk_file.filename
-    video_folder = '/app/data/videos'
-    video_file_path = os.path.join(video_folder, file_name)
-
-    chunk_folder = '/app/data/videos/chunks'
-    if not os.path.exists(chunk_folder):
-        os.makedirs(chunk_folder)
-
-    chunk_file_path = os.path.join(chunk_folder, f'{file_name}.{chunk_index}')
-
-    chunk_file.save(chunk_file_path)
-
-    if int(chunk_index) == int(total_chunks) - 1:
-
-        with open(video_file_path, 'wb') as output_file:
-            for i in range(int(total_chunks)):
-                chunk_path = os.path.join(chunk_folder, f'{file_name}.{i}')
-                with open(chunk_path, 'rb') as input_file:
-                    output_file.write(input_file.read())
-
-                os.remove(chunk_path)
-
-    return jsonify({'url': video_file_path})
-
-
-""" @app.route('/upload_image', methods=['POST'])
-def upload_poster():
-    file = request.files['file']
-    # generate a UUID4
-    filename = 'image' + str(uuid.uuid4())
-    filepath = os.path.join('/app/data/images', filename)
-    file.save(filepath)
-    response = {
-        'success': True,
-        "message": "File uploaded successfully",
-        "url": filepath,
-    }
-    return response """
-
+        return jsonify({'error': 'No file found'})
 
 if __name__ == '__main__':
     app.run()
