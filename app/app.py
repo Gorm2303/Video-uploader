fcc81e7854102908e2956629fc89cdd6d534d2fb
Gorm2303 <gormdk333@gmail.com>
2023-03-29 18:35:10 +0200

    Video and poster API are expecting chunks to be send

--- app/app.py
+++ app/app.py
@@ -36,19 +36,35 @@ def upload_video_metadata():
 @app.route('/poster', methods=['POST'])
 @cross_origin()
 def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-    file = request.files['file']
-    if file:
-        # generate a UUID4
-        filename = 'image_' + str(uuid.uuid4())
-        file_path = os.path.join('/app/data/images', filename)
-        file.save(file_path)
-        return jsonify({'url': file_path})
-    else:
-        return jsonify({'error': 'No file found'})
+    chunk_index = request.form['chunkIndex']
+    total_chunks = request.form['chunks']
+    chunk_file = request.files['chunk']
+    file_name = chunk_file.filename
+    image_folder = '/app/data/images'
+    image_name = 'image_' + str(uuid.uuid4())
+    image_file_path = os.path.join(image_folder, image_name)
 
-@app.route('/video', methods=['POST'])
+    chunk_folder = '/app/data/images/chunks'
+    if not os.path.exists(chunk_folder):
+        os.makedirs(chunk_folder)
+
+    chunk_file_path = os.path.join(chunk_folder, f'{file_name}.{chunk_index}')
+
+    chunk_file.save(chunk_file_path)
+
+    if int(chunk_index) == int(total_chunks) - 1:
+
+        with open(image_file_path, 'wb') as output_file:
+            for i in range(int(total_chunks)):
+                chunk_path = os.path.join(chunk_folder, f'{file_name}.{i}')
+                with open(chunk_path, 'rb') as input_file:
+                    output_file.write(input_file.read())
+
+                os.remove(chunk_path)
+
+    return jsonify({'url': image_file_path})
+
+""" @app.route('/video', methods=['POST'])
 @cross_origin()
 def upload_video():
     file = request.files['file']
@@ -59,7 +75,38 @@ def upload_video():
         file.save(file_path)
         return jsonify({'url': file_path})
     else:
-        return jsonify({'error': 'No file found'})
+        return jsonify({'error': 'No file found'}) """
+    
+@app.route('/video', methods=['POST'])
+@cross_origin()
+def upload_video():
+    chunk_index = request.form['chunkIndex']
+    total_chunks = request.form['chunks']
+    chunk_file = request.files['chunk']
+    file_name = chunk_file.filename
+    video_folder = '/app/data/videos'
+    video_name = 'video_' + str(uuid.uuid4())
+    video_file_path = os.path.join(video_folder, video_name)
+
+    chunk_folder = '/app/data/videos/chunks'
+    if not os.path.exists(chunk_folder):
+        os.makedirs(chunk_folder)
+
+    chunk_file_path = os.path.join(chunk_folder, f'{file_name}.{chunk_index}')
+
+    chunk_file.save(chunk_file_path)
+
+    if int(chunk_index) == int(total_chunks) - 1:
+
+        with open(video_file_path, 'wb') as output_file:
+            for i in range(int(total_chunks)):
+                chunk_path = os.path.join(chunk_folder, f'{file_name}.{i}')
+                with open(chunk_path, 'rb') as input_file:
+                    output_file.write(input_file.read())
+
+                os.remove(chunk_path)
+
+    return jsonify({'url': video_file_path})
 
 if __name__ == '__main__':
     app.run()
