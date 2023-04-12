dd6d665ae163656b9b28d5b8017f73a9aa5f3b2a
Gorm2303 <gormdk333@gmail.com>
2023-04-12 21:54:09 +0200

    Refactored video and poster API
    They had too much in common

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask, request, jsonify, flash, Response
+from flask import Flask, request, jsonify
 import os
 import uuid
 
@@ -31,50 +31,43 @@ def upload_video_metadata():
     return jsonify(response)
 
 @app.route('/api/v1/poster', methods=['POST'])
-@cross_origin()
 def upload_image():
-    chunk_index = request.form['chunkIndex']
-    total_chunks = request.form['chunks']
-    chunk_file = request.files['chunk']
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg']
     file_name = request.form['filename']
-    chunk_name = chunk_file.filename
-    image_folder = '/data/images'
-    image_name = 'image_' + str(uuid.uuid4()) + os.path.splitext(file_name)[1]
-    image_file_path = os.path.join(image_folder, image_name)
+    file_ext = os.path.splitext(file_name)[1]
 
-    chunk_folder = '/data/images/chunks'
-    if not os.path.exists(chunk_folder):
-        os.makedirs(chunk_folder)
+    if file_ext.lower() in image_extensions:
+        file_folder = '/data/images'
+        file_prefix = 'image_'
+    else:
+        return jsonify({'error': 'Invalid file type'})
 
-    chunk_file_path = os.path.join(chunk_folder, f'{chunk_name}.{chunk_index}')
-
-    chunk_file.save(chunk_file_path)
-
-    if int(chunk_index) == int(total_chunks) - 1:
-
-        with open(image_file_path, 'wb') as output_file:
-            for i in range(int(total_chunks)):
-                chunk_path = os.path.join(chunk_folder, f'{chunk_name}.{i}')
-                with open(chunk_path, 'rb') as input_file:
-                    output_file.write(input_file.read())
-
-                os.remove(chunk_path)
-
-    return jsonify({'url': image_file_path})
+    return upload_file(request, file_folder, file_prefix, file_ext)
     
 @app.route('/api/v1/video', methods=['POST'])
-@cross_origin()
 def upload_video():
+    video_extensions = ['.mp4', '.avi', '.mov', '.wmv', '.mkv', '.flv', '.webm']
+    file_name = request.form['filename']
+    file_ext = os.path.splitext(file_name)[1]
+    
+    if file_ext.lower() in video_extensions:
+        file_folder = '/data/videos'
+        file_prefix = 'video_'
+    else:
+        return jsonify({'error': 'Invalid file type'})
+
+    return upload_file(request, file_folder, file_prefix, file_ext)
+
+
+def upload_file(request, file_folder, file_prefix, file_ext):
     chunk_index = request.form['chunkIndex']
     total_chunks = request.form['chunks']
     chunk_file = request.files['chunk']
-    file_name = request.form['filename']
     chunk_name = chunk_file.filename
-    video_folder = '/data/videos'
-    video_name = 'video_' + str(uuid.uuid4()) + os.path.splitext(file_name)[1]
-    video_file_path = os.path.join(video_folder, video_name)
+    
+    file_path = os.path.join(file_folder, file_prefix + str(uuid.uuid4()) + file_ext)
 
-    chunk_folder = '/data/videos/chunks'
+    chunk_folder = os.path.join(file_folder, 'chunks')
     if not os.path.exists(chunk_folder):
         os.makedirs(chunk_folder)
 
@@ -84,7 +77,7 @@ def upload_video():
 
     if int(chunk_index) == int(total_chunks) - 1:
 
-        with open(video_file_path, 'wb') as output_file:
+        with open(file_path, 'wb') as output_file:
             for i in range(int(total_chunks)):
                 chunk_path = os.path.join(chunk_folder, f'{chunk_name}.{i}')
                 with open(chunk_path, 'rb') as input_file:
@@ -92,7 +85,7 @@ def upload_video():
 
                 os.remove(chunk_path)
 
-    return jsonify({'url': video_file_path})
+    return jsonify({'url': file_path})
 
 if __name__ == '__main__':
     app.run()
